4bfb32a1bb9791ddd92e0eec9077e05d3a873068
3runo <bruno.adex@gmail.com>
2020-02-08 15:48:38 +0100

    Adding react-bootstrap component library

---
+++ src/types/modules.d.ts
@@ -0,0 +1,30 @@
+declare module "*.ico" {
+  const path: string;
+  export = path;
+}
+
+declare module "*.jpg" {
+  const path: string;
+  export = path;
+}
+
+declare module "*.jpeg" {
+  const path: string;
+  export = path;
+}
+
+declare module "*.png" {
+  const path: string;
+  export = path;
+}
+
+declare module "*.svg" {
+  const path: string;
+  export = path;
+}
+
+declare module "*.svgx" {
+  import * as React from "react";
+  const element: React.ComponentClass<React.SVGAttributes<SVGElement>>;
+  export default element;
+}
