cf0a63b8fdf7303ef7a19e56df5941146f2dfd91
3runo <bruno.adex@gmail.com>
2020-02-10 22:24:51 +0100

    Adding routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Header from "./views/header";
+import PatientForm from "./views/patient-form";
 import PatientList from "./views/patient-list";
+
+function Index() {
+  return <h4>Home</h4>;
+}
+
+function Detail() {
+  return <h4>Detail</h4>;
+}
+
+function Calendar() {
+  return <h4>Calendar</h4>;
+}
 
 export default function App() {
   return (
-    <Container>
-      <Header />
-      <PatientList />
-    </Container>
+    <Router>
+      <Container>
+        <Header />
+        <Route path="/" exact component={Index} />
+        <Route path="/add-patients/" component={PatientForm} />
+        <Route path="/calendar/" component={Calendar} />
+        <Route path="/patient/:id" component={Detail} />
+        <Route path="/patients/" component={PatientList} />
+      </Container>
+    </Router>
   );
 }
--- src/views/header/index.tsx
+++ src/views/header/index.tsx
@@ -1,20 +1,28 @@
 import React from "react";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <>
-      <Navbar bg="light" expand="lg" className="mb-3">
-        <Navbar.Brand href="#home">Michele Ker</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#patients">Patients</Nav.Link>
-            <Nav.Link href="#calendar">Calendar</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </>
+    <Navbar bg="light" expand="lg" className="mb-3">
+      <Navbar.Brand>
+        <Link to="/">Michele Ker</Link>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Link to="/add-patients/" className="nav-link" role="button">
+            Add patient
+          </Link>
+          <Link to="/patients/" className="nav-link" role="button">
+            Patients
+          </Link>
+          <Link to="/calendar/" className="nav-link" role="button">
+            Calendar
+          </Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
